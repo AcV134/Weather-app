0a8a2038b9e2782589626e4612c58e33937210de
AcV134 <anirudh.c.vinayan@gmail.com>
2024-08-15 21:28:37 +0530

    Create Search function to get lat and log from name

--- index.js
+++ index.js
@@ -7,10 +7,27 @@ const port = 3000;
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json()); 
+
+let value;
 
 app.get('/',(req,res)=>{
-    res.render('index.ejs');
+    res.render('index.ejs',{result:value});
 })
+
+app.post('/search',async (req,res)=>{
+
+    // console.log(req.body);
+    const latitude = req.body.latitude;
+    const longitude = req.body.longitude;
+    try{
+        const result = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,apparent_temperature,weather_code&hourly=temperature_2m,apparent_temperature,weather_code&daily=weather_code&timezone=auto#`);
+        value = result.data;
+        res.redirect('/');
+    }catch(error){
+        console.error(error);
+    }
+});
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
--- public/src/script.js
+++ public/src/script.js
@@ -1,3 +1,38 @@
-// Add your code here
 
-console.log('working');
+let latitude;
+let longitude;
+
+$("#location").on("input", async function(event) {
+    try{
+        const result = await axios.get(`https://geocoding-api.open-meteo.com/v1/search?name=${event.target.value}&count=10&language=en&format=json`);
+        const location = (result.data).results;
+        $("#options").empty();
+        location.forEach(function(location){
+            $("#options").append(`<option value="${location.latitude} ${location.longitude}">${location.name}, ${location.country}</option>`);
+        });
+    }catch(error){
+        console.error(error);
+    }
+    console.log(event.target.value);
+});
+
+
+$("#options").on('click',(e)=>{
+    let values = e.target.value.split(" ");
+    latitude = values[0];
+    longitude = values[1];
+    $("#location").val(e.target.innerHTML);
+    $("#options").empty();
+})
+
+$("#form").on("submit", async function(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+    try{
+        const result = await axios.post('/search',{
+            latitude: latitude,
+            longitude: longitude
+        })
+    }catch(error){
+        console.error(error);
+    }
+});
